f8f57c8205a7b1e3c68ec8f424f688d19268eb45
Ammar Ahmed <ammar.ahmed1@uwaterloo.ca>
2024-01-12 22:45:08 -0500

    Testing git hooks

--- .gitHooks/hooks/post-commit.js
+++ .gitHooks/hooks/post-commit.js
@@ -1,12 +1,24 @@
 const fs = require("fs");
 const path = require("path");
 const readline = require("readline");
+const child_process = require("child_process");
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 })
 
-rl.question("Would you like to tag this commit? ", (res) => {
+const getTags = () => {
+  const res = child_process.execSync("git tag").toString();
+  console.log(res);
+}
+
+getTags();
+
+// rl.question("Would you like to tag this commit?[y/N] ", (res) => {
   
-})
+// })
+
+// rl.on("close", () => {
+
+// })
